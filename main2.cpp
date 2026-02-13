0aae8ebf863ecc1778ae16ab8064bcf58b4f88bb
Swerbakov <126806396+Swerbakov@users.noreply.github.com>
2026-02-13 18:12:07 +0300

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double number;
+    
+    cout << "Введите число: " << endl;
+    cin >> number;
+    
+    cout << "Вы ввели: " << endl;
+    cout << number << endl;
+    
+    return 0;
+}
